e19faddd03ed9e78d85ca479eb5affb798b0c4d6
PETER IRUNGU <peteralidante254@gmail.com>
2023-11-23 11:02:52 +0300

    highlight wrong words and give suggstions

--- agent/checker.py
+++ agent/checker.py
@@ -1,5 +1,5 @@
-from . import data
 import re
+from . import data
 
 swahili_dict = data.load_swahili_dictionary("kamusi/words.json")
 
@@ -8,17 +8,21 @@ def search_word(user_word, swahili_dict):
     user_word = user_word.lower()
 
     for word_id, word_info in swahili_dict.items():
-        if user_word in word_info["Word"].lower():
+        if re.match(rf"\b{re.escape(user_word)}\b", word_info["Word"].lower()):
             return f"'{user_word}' is found in the Swahili dictionary. Meaning: {word_info['Meaning']}"
 
     for word_id, word_info in swahili_dict.items():
         conjugation = word_info.get("Conjugation")
-        if conjugation and user_word in conjugation.lower():
+        if conjugation and re.match(
+            rf"\b{re.escape(user_word)}\b", conjugation.lower()
+        ):
             return f"'{user_word}' is found in a word with conjugations. Original Word: {word_info['Word']}. Meaning: {word_info['Meaning']}"
-        elif conjugation is None and user_word in word_info["Word"].lower():
+        elif conjugation is None and re.match(
+            rf"\b{re.escape(user_word)}\b", word_info["Word"].lower()
+        ):
             return f"'{user_word}' is found in a word without conjugations. Meaning: {word_info['Meaning']}"
 
-    return f"'{user_word}' is not found in the Swahili dictionary or any word with conjugations."
+    return f"'{user_word}' not found in the Swahili dictionary or any word with conjugations."
 
 
 def check_sentence_words(user_input):
@@ -29,17 +33,16 @@ def check_sentence_words(user_input):
     incorrect_word_indexes = []
 
     for word in words:
-      start_index = user_input.find(word)
-      end_index = start_index + len(word)
-      words_with_indexes.append(
+        start_index = user_input.find(word)
+        end_index = start_index + len(word)
+        words_with_indexes.append(
             {"word": word, "start_index": start_index, "end_index": end_index}
-      )
+        )
 
-      result = search_word(word, swahili_dict)
-      if "not found" in result.lower():
+        result = search_word(word, swahili_dict)
+        if "not found" in result.lower():
             incorrect_word_indexes.append(start_index)
-
-
+        
     if incorrect_word_indexes:
         return incorrect_word_indexes
     else:
@@ -47,5 +50,8 @@ def check_sentence_words(user_input):
 
 
 # Example usage
+user_input = "Your input sentence here"
+result = check_sentence_words(user_input)
+print(result)
 
 check_sentence_words
--- res/ui.py
+++ res/ui.py
@@ -1,31 +1,59 @@
 import tkinter as tk
 import agent.checker as agent
+import agent.suggestions as sg
 import json
 
 
 def on_button_click():
-    input_text = text.get(
-        "1.0", tk.END
-    ).strip()  # Get the entire content of the Text widget
+    input_text = text.get("1.0", tk.END).strip()
     textview.config(state=tk.NORMAL)
     textview.insert(tk.END, f"Umeandika: {input_text}\n")
     textview.config(state=tk.DISABLED)
 
 
-def update_label(event):
+def get_current_word(text_widget):
+    cursor_position = text_widget.index(tk.INSERT)
+    start = text_widget.search(r"\s", cursor_position, backwards=True, regexp=True)
+    if start == "":
+        start = "1.0"
+    else:
+        start = text_widget.index(f"{start}+1c")
+    end = text_widget.search(r"\s", cursor_position, regexp=True)
+    if end == "":
+        end = tk.END
+    return text_widget.get(start, end).strip()
+
+
+def highlight_wrong_words(event):
+    text.after(500, update_label)
+
+
+def update_label():
     input_text = text.get("1.0", tk.END).strip()
     wrong_word_indexes = agent.check_sentence_words(input_text)
-    
+
     # Remove any existing tags
     text.tag_delete("underline")
 
-    sentence_word_list = input_text.split(' ')
-    # Apply red underline to the incorrect words
-    for index in wrong_word_indexes:
-        start = f"1.{index}"
-        end = f"{start} wordend"
-        text.tag_add("underline", start, end)
-        text.tag_configure("underline", underline=True, foreground="red")
+    # Get suggestions for the current word
+    current_word = get_current_word(text)
+    suggestions = sg.get_suggestions(current_word)
+
+    # Update the label with suggestions
+    label.config(text=f"Suggestions: {', '.join(suggestions)}")
+
+    sentence_word_list = input_text.split(" ")
+
+    # Check if any suggestion starts with the current word
+    starts_with_current_word = any(s.startswith(current_word) for s in suggestions)
+
+    # Apply red underline to the incorrect words if no suggestion starts with the current word
+    if not starts_with_current_word:
+        for index in wrong_word_indexes:
+            start = f"1.{index}"
+            end = f"{start} wordend"
+            text.tag_add("underline", start, end)
+            text.tag_configure("underline", underline=True, foreground="red")
 
 
 def apply_styles(widget, styles):
@@ -38,48 +66,34 @@ def start_app():
     global label
     global text
 
-    # Read UI configuration from JSON file
     with open("ui_config.json", "r") as config_file:
         ui_config = json.load(config_file)
 
-    # Create the main window
     window = tk.Tk()
-
-    # Maximize the window
     window.state("zoomed")
     window.title("Swahili Auto Correct")
-
-    # Set dark background color
     window.configure(bg=ui_config["entry"]["bg"])
-
-    # Center the window elements
     window.columnconfigure(0, weight=1)
     window.rowconfigure(0, weight=1)
 
-    # Create text input box
     text = tk.Text(window, wrap=tk.WORD)
     apply_styles(text, ui_config["entry"])
     text.grid(row=0, column=0, padx=20, pady=10, sticky="nsew")
-    text.bind("<KeyRelease>", update_label)
+    text.bind("<KeyRelease>", highlight_wrong_words)
 
-    # Create show text box (label)
     label = tk.Label(window, text="")
     apply_styles(label, ui_config["label"])
     label.grid(row=1, column=0, padx=20, pady=10, sticky="nsew")
 
-    # Create a button
     button = tk.Button(window, text="Submit", command=on_button_click)
     apply_styles(button, ui_config["button"])
     button.grid(row=2, column=0, padx=20, pady=10, sticky="nsew")
 
-    # Create a textview box
     textview = tk.Text(window, wrap=tk.WORD)
     apply_styles(textview, ui_config["textview"])
     textview.grid(row=3, column=0, padx=20, pady=10, sticky="nsew")
 
-    # Run the Tkinter event loop
     window.mainloop()
 
 
-# Call the function to create the maximized window
 start_app()
